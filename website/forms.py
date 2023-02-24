23f47d15127e621162a88220a073f9e80aafccd5
alexandr <alerusford@gmail.com>
2023-02-24 12:21:50 +0300

    auth and register - ok!

---
+++ website/forms.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="",
+                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    first_name = forms.CharField(label="", max_length=100,
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=100,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted">' \
+                                            '<small>Поле до 150 символов, можно использовать буквы, ' \
+                                            'цифры и @/./+/-/_.</small></span>'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small">' \
+                                             '<li>Пароль не может быть похож на логин!</li>' \
+                                             '<li>Пароль должен содержать не менее 8 символов!</li>' \
+                                             '<li>Пароль не может быть часто используемым паролем!</li>' \
+                                             '<li>Пароль не может быть полностью числовым!</li></ul>'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted">' \
+                                             '<small>Повторите пароль.</small></span>'
+
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     if request.method == 'POST':
@@ -9,13 +10,37 @@ def home(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, 'Вы успешно вошли!')
+            return redirect('home')
+        else:
+            messages.success(request, 'Произошла ошибка, попробуйте еще раз ...')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
-
-    return render(request, 'home.html', {})
-
-def login_user(request):
-    pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'Вы вышли ...')
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Вы успешно зарегистрировались! Приветствую, {username}!')
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
+
 
